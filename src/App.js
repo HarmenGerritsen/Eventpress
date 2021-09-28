4805a6f9c7560964547ce2a2996c67be57750d86
Harmen <hgautospotter@gmail.com>
2021-09-28 16:22:06 +0200

    fixed some minor stuff

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import fe1 from "./frontend1.png";
 function App() {
   const [show1, setShow1] = useState(false);
   const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
 
   const handleClose1 = () => setShow1(false);
   const handleShow1 = () => setShow1(true);
 
   const handleClose2 = () => setShow2(false);
   const handleShow2 = () => setShow2(true);
+
+  const handleClose3 = () => setShow3(false);
+  const handleShow3 = () => setShow3(true);
 
   return (
     <div className="App">
@@ -35,53 +39,74 @@ function App() {
       </div>
 
       <div>
-      <Modal show={show1} onHide={handleClose1}>
+        <Modal show={show1} onHide={handleClose1}>
 
-        <Modal.Header className="popup">
-          <Modal.Title className="ModalTitle">Evenement aanmaken</Modal.Title>
-          <button className="closeButton" onClick={handleClose1}>X</button>
-        </Modal.Header>
+          <Modal.Header className="popup">
+            <Modal.Title className="ModalTitle">Evenement aanmaken</Modal.Title>
+            <button className="closeButton" onClick={handleClose1}>X</button>
+          </Modal.Header>
 
-        <Modal.Body>
-          <textarea className="textarea" placeholder="Vul hier je event informatie in..."></textarea>
-        </Modal.Body>
+          <Modal.Body>
+            <textarea className="textarea eventtextarea" placeholder="Vul hier je event informatie in..."></textarea>
+          </Modal.Body>
 
-        <Modal.Footer class="col text-center">
-          <button  type="button" class="btn btn-dark button" variant="secondary" onClick={handleClose1}>
-            Sluiten
-          </button>
-          <br />
-          <p className="white">.</p>
-        </Modal.Footer>
+          <Modal.Footer class="col text-center">
+            <button type="button" class="btn btn-dark button" variant="secondary" onClick={handleClose1}>
+              Sluiten
+            </button>
+            <br />
+            <p className="white">.</p>
+          </Modal.Footer>
 
-      </Modal>
+        </Modal>
 
-      <Modal show={show2} onHide={handleClose2}>
+        <Modal show={show2} onHide={handleClose2}>
 
-        <Modal.Header className="popup">
-          <Modal.Title className="ModalTitle">Evenement inzien</Modal.Title>
-          <button className="closeButton" onClick={handleClose2}>X</button>
-        </Modal.Header>
+          <Modal.Header className="popup">
+            <Modal.Title className="ModalTitle">Evenement inzien</Modal.Title>
+            <button className="closeButton" onClick={handleClose2}>X</button>
+          </Modal.Header>
 
-        <Modal.Body>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at rhoncus nunc, faucibus sagittis erat. Sed quis iaculis sem. Etiam sed elementum dolor, ut bibendum nunc. Sed vel mollis risus, nec consectetur dui. Aliquam finibus molestie arcu eget ornare. Vestibulum in dolor nisl. Sed quis egestas tellus.
-          Nunc dolor ligula, hendrerit a enim nec, aliquet semper ipsum. Pellentesque massa dolor, venenatis at molestie nec, bibendum eu metus. Suspendisse egestas mattis varius. Maecenas id fringilla diam. Nulla placerat vehicula aliquam. Aliquam erat volutpat. Nam finibus sodales auctor. Aliquam ultrices mi sed lacus aliquam ultrices sed eu lectus. Vestibulum maximus nisl velit, sed tristique magna tristique et. Praesent sit amet rhoncus est, nec suscipit massa. Suspendisse potenti. Integer ex turpis, pretium ac dolor et, vulputate lobortis ante. Sed eu consectetur justo, vitae semper nulla. Vestibulum volutpat risus quis erat ultrices aliquam.
-          </p>
-        </Modal.Body>
+          <Modal.Body>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at rhoncus nunc, faucibus sagittis erat. Sed quis iaculis sem. Etiam sed elementum dolor, ut bibendum nunc. Sed vel mollis risus, nec consectetur dui. Aliquam finibus molestie arcu eget ornare. Vestibulum in dolor nisl. Sed quis egestas tellus.
+              Nunc dolor ligula, hendrerit a enim nec, aliquet semper ipsum. Pellentesque massa dolor, venenatis at molestie nec, bibendum eu metus. Suspendisse egestas mattis varius. Maecenas id fringilla diam. Nulla placerat vehicula aliquam. Aliquam erat volutpat. Nam finibus sodales auctor. Aliquam ultrices mi sed lacus aliquam ultrices sed eu lectus. Vestibulum maximus nisl velit, sed tristique magna tristique et. Praesent sit amet rhoncus est, nec suscipit massa. Suspendisse potenti. Integer ex turpis, pretium ac dolor et, vulputate lobortis ante. Sed eu consectetur justo, vitae semper nulla. Vestibulum volutpat risus quis erat ultrices aliquam.
+            </p>
+          </Modal.Body>
 
-        <Modal.Footer class="col text-center">
-          <button type="button" class="btn btn-dark button" variant="secondary">
-            Inschrijven
-          </button>
-          <button  type="button" class="btn btn-dark button" variant="secondary" onClick={handleClose2}>
-            Sluiten
-          </button>
-          <br />
-          <p className="white">.</p>
-        </Modal.Footer>
+          <Modal.Footer class="col text-center">
+            <button type="button" class="btn btn-dark button" variant="secondary" onClick={() => { handleClose2(); handleShow3(); }}>
+              Inschrijven
+            </button>
+            <button type="button" class="btn btn-dark button" variant="secondary" onClick={handleClose2}>
+              Sluiten
+            </button>
+            <br />
+            <p className="white">.</p>
+          </Modal.Footer>
 
-      </Modal>
+        </Modal>
+
+        <Modal show={show3} onHide={handleClose3}>
+
+          <Modal.Header className="popup">
+            <Modal.Title className="ModalTitle">Inschrijven</Modal.Title>
+            <button className="closeButton" onClick={handleClose3}>X</button>
+          </Modal.Header>
+
+          <Modal.Body>
+            <textarea className="textarea" placeholder="vul je mailadres in..."></textarea>
+          </Modal.Body>
+
+          <Modal.Footer class="col text-center">
+            <button type="button" class="btn btn-dark button" variant="secondary" onClick={handleClose3}>
+              Verzenden
+            </button>
+            <br />
+            <p className="white">.</p>
+          </Modal.Footer>
+
+        </Modal>
       </div>
 
     </div>
