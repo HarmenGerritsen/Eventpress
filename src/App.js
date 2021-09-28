76e94c7ef1b36e0fb770f14f1b8157aecd53c922
Harmen <hgautospotter@gmail.com>
2021-09-28 12:52:52 +0200

    Added button and popup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
+import plus from "./plus.png";
+import Popup from 'reactjs-popup';
 import './App.css';
+import 'reactjs-popup/dist/index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p className="title">
+      <div className="App-header">
+          <p className="title">
           Eventpress
-        </p>
-      </header>
+          </p>
+          <Popup trigger={<a href="#"><img className="plus" src={plus} alt=''></img></a>} position="right center">
+            <div classNam="popup">
+              popup!
+            </div>
+          </Popup>
+      </div>
     </div>
   );
 }
