39bac35469c4a92ac7e459069352a6bddd25c72e
Harmen <hgautospotter@gmail.com>
2021-09-29 14:27:30 +0200

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import plus from "./plus.png";
 import Modal from "react-bootstrap/Modal";
+import React from "react";
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import fe1 from "./frontend1.png";
-import React from "react";
-import { useState } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import po1 from "./productowner1.png";
 import ai1 from "./artint1.png";
 import be1 from "./backend1.png";
 import csc1 from "./cloud1.png";
+import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 function App() {
@@ -68,101 +69,74 @@ function App() {
               <img onClick={handleShow2} className="fe1" src={fe1} alt=''></img>
             </a>
           </Col>
-          <Col style={{
-        }}>
+          <Col style={{}}>
           <a href="#">
-          <img onClick={handleShow2} className="be1" src={be1} alt=''>
-          </img>
-        </a>
+            <img onClick={handleShow2} className="be1" src={be1} alt=''></img>
+          </a>
         </Col>
-          <Col style={{
-          }}>
+          <Col style={{}}>
           <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
-          </img>
-        </a>
+            <img onClick={handleShow2} className="fe1" src={fe1} alt=''></img>
+          </a>
         </Col>
-        <Col style={{
-          }}>
+        <Col style={{}}>
           <a href="#">
-          <img onClick={handleShow2} className="be1" src={be1} alt=''>
-          </img>
-        </a>
-        </Col>
-        </Row>
-
-        <Row classname="row">
-          <Col style={{
-          }}>
-                    <a href="#">
-          <img onClick={handleShow2} className="po1" src={po1} alt=''>
-          </img>
-        </a>
-        </Col>
-        <Col style={{
-          }}>
-                    <a href="#">
-          <img onClick={handleShow2} className="ai1" src={ai1} alt=''>
-          </img>
-        </a>
-        </Col>
-          <Col style={{
-        }}>
-          <a href="#">
-          <img onClick={handleShow2} className="csc1" src={csc1} alt=''>
-          </img>
-        </a>
-        </Col>
-          <Col style={{
-          }}>
-          <a href="#">
-          <img onClick={handleShow2} className="ai1" src={ai1} alt=''>
-          </img>
-        </a>
-        </Col>
-        <Col style={{
-          }}>
-          <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
-          </img>
-        </a>
+            <img onClick={handleShow2} className="be1" src={be1} alt=''></img>
+          </a>
         </Col>
         </Row>
         <Row classname="row">
-          <Col style={{
-          }}>
-                    <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
-          </img>
-        </a>
-        </Col>
-        <Col style={{
-          }}>
-                    <a href="#">
-          <img onClick={handleShow2} className="be1" src={be1} alt=''>
-          </img>
-        </a>
-        </Col>
-          <Col style={{
-        }}>
+        <Col style={{}}>
           <a href="#">
-          <img onClick={handleShow2} className="csc1" src={csc1} alt=''>
-          </img>
-        </a>
+            <img onClick={handleShow2} className="po1" src={po1} alt=''></img>
+          </a>
         </Col>
-          <Col style={{
-          }}>
+        <Col style={{}}>
           <a href="#">
-          <img onClick={handleShow2} className="ai1" src={ai1} alt=''>
-          </img>
-        </a>
+            <img onClick={handleShow2} className="ai1" src={ai1} alt=''></img>
+          </a>
         </Col>
-        <Col style={{
-          }}>
+        <Col style={{}}>
           <a href="#">
-          <img onClick={handleShow2} className="po1" src={po1} alt=''>
-          </img>
-        </a>
+            <img onClick={handleShow2} className="csc1" src={csc1} alt=''></img>
+          </a>
+        </Col>
+        <Col style={{}}>
+          <a href="#">
+            <img onClick={handleShow2} className="ai1" src={ai1} alt=''></img>
+          </a>
+        </Col>
+        <Col style={{}}>
+          <a href="#">
+            <img onClick={handleShow2} className="fe1" src={fe1} alt=''></img>
+          </a>
+        </Col>
+        </Row>
+        <Row classname="row">
+          <Col style={{}}>
+           <a href="#">
+            <img onClick={handleShow2} className="fe1" src={fe1} alt=''></img>
+          </a>
+        </Col>
+        <Col style={{}}>
+           <a href="#">
+            <img onClick={handleShow2} className="be1" src={be1} alt=''></img>
+          </a>
+        </Col>
+        <Col style={{}}>
+          <a href="#">
+            <img onClick={handleShow2} className="csc1" src={csc1} alt=''></img>
+          </a>
+        </Col>
+        <Col style={{}}>
+          <a href="#">
+            <img onClick={handleShow2} className="ai1" src={ai1} alt=''></img>
+          </a>
+        </Col>
+        <Col style={{}}>
+          <a href="#">
+            <img onClick={handleShow2} className="po1" src={po1} alt=''></img>
+          </a>
         </Col>
         </Row>
       </div>
