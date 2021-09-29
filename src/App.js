6fa9c17e062ec1b36be11720e7680803ab883285
Davud <davud.susam@student.hu.nl>
2021-09-29 10:45:55 +0200

    fotosedit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import po1 from "./productowner1.png";
+import ai1 from "./artint1.png";
+import be1 from "./backend1.png";
+import csc1 from "./cloud1.png";
 
 
 function App() {
@@ -51,7 +55,7 @@ function App() {
           <Col style={{
           }}>
                     <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="po1" src={po1} alt=''>
           </img>
         </a>
         </Col>
@@ -65,7 +69,7 @@ function App() {
           <Col style={{
         }}>
           <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="be1" src={be1} alt=''>
           </img>
         </a>
         </Col>
@@ -79,7 +83,7 @@ function App() {
         <Col style={{
           }}>
           <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="be1" src={be1} alt=''>
           </img>
         </a>
         </Col>
@@ -89,28 +93,28 @@ function App() {
           <Col style={{
           }}>
                     <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="po1" src={po1} alt=''>
           </img>
         </a>
         </Col>
         <Col style={{
           }}>
                     <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="ai1" src={ai1} alt=''>
           </img>
         </a>
         </Col>
           <Col style={{
         }}>
           <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="csc1" src={csc1} alt=''>
           </img>
         </a>
         </Col>
           <Col style={{
           }}>
           <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="ai1" src={ai1} alt=''>
           </img>
         </a>
         </Col>
@@ -133,28 +137,28 @@ function App() {
         <Col style={{
           }}>
                     <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="be1" src={be1} alt=''>
           </img>
         </a>
         </Col>
           <Col style={{
         }}>
           <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="csc1" src={csc1} alt=''>
           </img>
         </a>
         </Col>
           <Col style={{
           }}>
           <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="ai1" src={ai1} alt=''>
           </img>
         </a>
         </Col>
         <Col style={{
           }}>
           <a href="#">
-          <img onClick={handleShow2} className="fe1" src={fe1} alt=''>
+          <img onClick={handleShow2} className="po1" src={po1} alt=''>
           </img>
         </a>
         </Col>
